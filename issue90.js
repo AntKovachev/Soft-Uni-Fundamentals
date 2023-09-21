f285b8a2d9fb5ff2a81179786f2d4c1de1abcaef
Martin Ananiev <127123719+marto492@users.noreply.github.com>
2023-09-21 15:15:28 +0300

    Email adress validator Email Address Validator #90

---
+++ issue90.js
@@ -0,0 +1,13 @@
+const func = (email) => {
+  if (email.includes(`@`) && email.includes(`.`)) {
+    return true;
+  }
+  return false;
+};
+
+// ****CHECKS BELOW****
+// let emailtrue = func(`marto492@abv.bg`)
+// let emailfalse = func(`marto492abvbg`)
+
+// console.log(emailtrue);
+// console.log(emailfalse);
