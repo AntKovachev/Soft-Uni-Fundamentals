164247ba4097a584062c4268d042502db3fda316
SnowShine7 <82907196+SnowShine7@users.noreply.github.com>
2023-09-23 16:32:29 +0300

    Solution for #20

---
+++ cubeNum.js
@@ -0,0 +1,5 @@
+function cubeNum(a) {
+    let cube = a * a * a;
+    console.log(cube)
+}
+cubeNum(2)
--- primeNum.js
+++ primeNum.js
@@ -1,12 +1,12 @@
 function prime(a) {
     if (a <= 1) {
-        console.log(`not prime`);
+        console.log(`False`);
     }  else {
 
             if (a % 2 == 0) {
-                console.log(`not prime`);
+                console.log(`False`);
             } else if (a % 2 != 0) {
-                console.log(`prime`)
+                console.log(`True`)
             }
     }
 }
