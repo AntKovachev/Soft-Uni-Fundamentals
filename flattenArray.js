89609f4172654c563ea56d42e6a345643d86549b
VaibhavKambar7 <vaibhavkambar123@gmail.com>
2023-09-25 19:16:53 +0530

    added flatten array

---
+++ flattenArray.js
@@ -0,0 +1,19 @@
+function flattenArray(nestedArray) {
+    const flattenedArray = [];
+  
+    function flatten(arr) {
+      arr.forEach(item => {
+        if (Array.isArray(item)) {
+          flatten(item);
+        } else {
+          flattenedArray.push(item);
+        }
+      });
+    }
+  
+    flatten(nestedArray);
+    console.log(flattenedArray);
+  }
+  
+  flattenArray(nestedArray);
+  
