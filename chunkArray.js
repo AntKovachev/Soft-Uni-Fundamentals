0ba60fe86331e1545a22b43ee145a88efd9f7544
VaibhavKambar7 <vaibhavkambar123@gmail.com>
2023-09-26 22:48:14 +0530

    added chunking array

---
+++ chunkArray.js
@@ -0,0 +1,13 @@
+function chunkArray(arr, chunkSize) {
+    const chunkedArray = [];
+    let index = 0;
+
+    while (index < arr.length) {
+      chunkedArray.push(arr.slice(index, index + chunkSize));
+      index += chunkSize;
+    }
+
+    console.log(chunkedArray);
+  }
+
+  chunkArray(originalArray, chunkSize);
