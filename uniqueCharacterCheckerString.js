be900e6182f6b3f8a0ebec3b9587c93a1e545beb
VaibhavKambar7 <vaibhavkambar123@gmail.com>
2023-09-26 23:02:48 +0530

    solved Unique Character Checker in a String issue

---
+++ uniqueCharacterCheckerString.js
@@ -0,0 +1,15 @@
+function hasUniqueCharacters(str) {
+    const charCount = {};
+
+    for (let i = 0; i < str.length; i++) {
+        const char = str[i];
+        
+        if (charCount[char]) {
+            return false;
+        }
+
+        charCount[char] = true;
+    }
+
+    return true;
+}
