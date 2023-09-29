ae5507e7bb84cc195d9b62bc03a80d8ca070bd43
VaibhavKambar7 <vaibhavkambar123@gmail.com>
2023-09-29 22:41:03 +0530

    combined both functions into one

--- filterPalindromes.js
+++ filterPalindromes.js
@@ -1,14 +1,17 @@
-function isPalindrome(str) {
-    const cleanedStr = str.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
-    return cleanedStr === cleanedStr.split('').reverse().join('');
-}
-
 function filterPalindromes(arr) {
+    const isPalindrome = (str) => {
+      const cleanedStr = str.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
+      return cleanedStr === cleanedStr.split("").reverse().join("");
+    };
+  
     const palindromeArray = [];
+  
     for (let i = 0; i < arr.length; i++) {
-        if (isPalindrome(arr[i])) {
-            palindromeArray.push(arr[i]);
-        }
+      if (isPalindrome(arr[i])) {
+        palindromeArray.push(arr[i]);
+      }
     }
+  
     return palindromeArray;
-}
+  }
+  
